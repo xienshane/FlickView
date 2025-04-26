02358eac2212d126f8d4070962a341b4a21690c6
xienshane <onglovelyshane71@gmail.com>
2025-04-26 19:19:42 +0800

    UI - settings page only (more smooth and uniform ui)

--- app/src/main/java/com/android/flickview/activities/SettingsPageActivity.kt
+++ app/src/main/java/com/android/flickview/activities/SettingsPageActivity.kt
@@ -5,8 +5,12 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.flickview.R
@@ -18,7 +22,11 @@ class SettingsPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_page)
 
-        val buttonAppLang: Button = findViewById(R.id.button_app)
+        val rootView = findViewById<View>(android.R.id.content)
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        rootView.startAnimation(fadeIn)
+
+        val buttonAppLang: LinearLayout = findViewById(R.id.app)
         buttonAppLang.setOnClickListener {
             Log.e("Developer", "Button is clicked")
             Toast.makeText(this, "App Language page is clicked", Toast.LENGTH_LONG).show()
@@ -27,7 +35,7 @@ class SettingsPageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val buttonDeveloper: Button = findViewById(R.id.button_developer)
+        val buttonDeveloper: LinearLayout = findViewById(R.id.developer)
         buttonDeveloper.setOnClickListener {
             Log.e("Developer", "Button is clicked")
             Toast.makeText(this, "Developer page is clicked", Toast.LENGTH_LONG).show()
@@ -41,7 +49,7 @@ class SettingsPageActivity : AppCompatActivity() {
             finish() // Goes back to the previous page
         }
 
-        val buttonRegion: Button = findViewById(R.id.button_region)
+        val buttonRegion: LinearLayout = findViewById(R.id.button_region)
         buttonRegion.setOnClickListener {
             Log.e("Region", "Button is clicked")
             Toast.makeText(this, "Region Preferences page is clicked", Toast.LENGTH_LONG).show()
@@ -50,25 +58,25 @@ class SettingsPageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val buttonSubtitles: Button = findViewById(R.id.button_subtitles)
-        buttonSubtitles.setOnClickListener {
-            Log.d("Subtitles", "Subtitles button clicked");
-            Toast.makeText(this, "Subtitles settings clicked", Toast.LENGTH_SHORT).show();
+//        val buttonSubtitles: Button = findViewById(R.id.button_subtitles)
+//        buttonSubtitles.setOnClickListener {
+//            Log.d("Subtitles", "Subtitles button clicked");
+//            Toast.makeText(this, "Subtitles settings clicked", Toast.LENGTH_SHORT).show();
+//
+//            val intent = Intent(this, SubtitlePreferencesActivity::class.java)
+//            startActivity(intent)
+//        }
 
-            val intent = Intent(this, SubtitlePreferencesActivity::class.java)
-            startActivity(intent)
-        }
+//        val buttonDisplay: Button = findViewById(R.id.button_display)
+//        buttonDisplay.setOnClickListener {
+//            Log.d("Display", "Display button clicked");
+//            Toast.makeText(this, "Display settings clicked", Toast.LENGTH_SHORT).show();
+//
+//             val intent = Intent(this, DisplayActivity::class.java)
+//             startActivity(intent)
+//        }
 
-        val buttonDisplay: Button = findViewById(R.id.button_display)
-        buttonDisplay.setOnClickListener {
-            Log.d("Display", "Display button clicked");
-            Toast.makeText(this, "Display settings clicked", Toast.LENGTH_SHORT).show();
-
-             val intent = Intent(this, DisplayActivity::class.java)
-             startActivity(intent)
-        }
-
-        val buttonNotifications: Button = findViewById(R.id.button_notifications)
+        val buttonNotifications: LinearLayout = findViewById(R.id.button_notifications)
         buttonNotifications.setOnClickListener {
             Log.d("Notifications", "Notifications button clicked");
             Toast.makeText(this, "Notifications settings clicked", Toast.LENGTH_SHORT).show();
@@ -86,16 +94,16 @@ class SettingsPageActivity : AppCompatActivity() {
           //  startActivity(intent)
        // }
 
-        val buttonDownloads: Button = findViewById(R.id.button_downloads)
-        buttonDownloads.setOnClickListener {
-            Log.d("Downloads", "Downloads button clicked");
-            Toast.makeText(this, "Downloads settings clicked", Toast.LENGTH_SHORT).show();
+//        val buttonDownloads: Button = findViewById(R.id.button_downloads)
+//        buttonDownloads.setOnClickListener {
+//            Log.d("Downloads", "Downloads button clicked");
+//            Toast.makeText(this, "Downloads settings clicked", Toast.LENGTH_SHORT).show();
+//
+//            val intent = Intent(this, ClearDownloadsActivity::class.java)
+//            startActivity(intent)
+//        }
 
-            val intent = Intent(this, ClearDownloadsActivity::class.java)
-            startActivity(intent)
-        }
-
-        val buttonCache: Button = findViewById(R.id.button_cache)
+        val buttonCache: LinearLayout = findViewById(R.id.button_clear_cache)
         buttonCache.setOnClickListener {
             Log.d("Cache", "Cache button clicked")
             Toast.makeText(this, "Cache settings clicked", Toast.LENGTH_SHORT).show()
@@ -107,7 +115,7 @@ class SettingsPageActivity : AppCompatActivity() {
 
 
 
-        val buttonPrivacy: Button = findViewById(R.id.button_privacy)
+        val buttonPrivacy: LinearLayout = findViewById(R.id.button_privacy_policy)
         buttonPrivacy.setOnClickListener {
             Log.d("Privacy", "Privacy button clicked");
             Toast.makeText(this, "Privacy Policy clicked", Toast.LENGTH_SHORT).show();
@@ -116,7 +124,7 @@ class SettingsPageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val buttonCopyright: Button = findViewById(R.id.button_copyright)
+        val buttonCopyright: LinearLayout = findViewById(R.id.button_copyright)
         buttonCopyright.setOnClickListener {
             Log.d("Copyright", "Copyright button clicked");
             Toast.makeText(this, "Copyright Info clicked", Toast.LENGTH_SHORT).show();
@@ -125,7 +133,7 @@ class SettingsPageActivity : AppCompatActivity() {
              startActivity(intent)
         }
 
-        val buttonRate: Button = findViewById(R.id.button_rate)
+        val buttonRate: LinearLayout = findViewById(R.id.button_rate_us)
         buttonRate.setOnClickListener {
             Log.d("Rate", "Rate App button clicked");
             Toast.makeText(this, "Rate App clicked", Toast.LENGTH_SHORT).show();
